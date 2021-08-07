6562b352ad37a9e4f4196ae0853be4dc53bd25cf
Rick Varela <rickvarela91@gmail.com>
2021-08-07 13:06:11 -0700

    Update text

--- src/sections/About.js
+++ src/sections/About.js
@@ -4,10 +4,10 @@ export const About = () => {
       <div className='site-about-wrapper'>
         <h2 className='about-headline'>ABOUT</h2>
         <p>
-          Hey! I’m Rick a self-taught programmer who enjoys coding and taking on
-          the challenges of a new project. I strive to expand my knowledge and
-          push myself to create increasingly more effective web sites and
-          applications.
+          Hey! I’m Rick a mechanical engineer and self-taught web developer who
+          enjoys coding and taking on the challenges of a new project. I strive
+          to expand my knowledge and push myself to create increasingly more
+          effective web applications and hardware products.
         </p>
       </div>
     </div>
